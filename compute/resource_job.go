ecc8e20300c0accdec248ed751e86f6a9bb4d137
Serge Smertin <serge.smertin@databricks.com>
2020-12-02 12:54:34 +0100

    Added context to databricks_job

--- compute/resource_job.go
+++ compute/resource_job.go
@@ -11,10 +11,11 @@ import (
 
 	"github.com/databrickslabs/databricks-terraform/common"
 	"github.com/databrickslabs/databricks-terraform/internal"
+	"github.com/databrickslabs/databricks-terraform/internal/util"
 )
 
 // NewJobsAPI creates JobsAPI instance from provider meta
-func NewJobsAPI(m interface{}) JobsAPI {
+func NewJobsAPI(ctx context.Context, m interface{}) JobsAPI {
 	return JobsAPI{m.(*common.DatabricksClient), context.TODO()}
 }
 
@@ -129,223 +130,44 @@ var jobSchema = internal.StructToSchema(JobSettings{},
 			"Run Now in the Jobs UI or sending an API request to runNow."
 		s["max_concurrent_runs"].Description = "An optional maximum allowed number of " +
 			"concurrent runs of the job."
-		s["notebook_path"] = &schema.Schema{
-			Deprecated:    "Please migrate `notebook_path` to `notebook_task`, as it will be removed in version 0.3",
-			Description:   "Deprecated. Please use `notebook_task`.",
-			Type:          schema.TypeString,
-			Optional:      true,
-			ConflictsWith: []string{"jar_main_class_name", "spark_submit_parameters", "python_file"},
-		}
-		s["notebook_base_parameters"] = &schema.Schema{
-			Deprecated:  "Please migrate `notebook_base_parameters` to `notebook_task`, as it will be removed in version 0.3",
-			Description: "Deprecated. Please use `notebook_task`.",
-			Type:        schema.TypeMap,
-			Optional:    true,
-			Elem:        &schema.Schema{Type: schema.TypeString},
-		}
-		s["jar_uri"] = &schema.Schema{
-			Deprecated: "`jar_uri` is deprecated since 04/2016 and will be removed in version 0.3",
-			Type:       schema.TypeString,
-			Optional:   true,
-		}
-		s["jar_main_class_name"] = &schema.Schema{
-			Deprecated:    "Please migrate `jar_main_class_name` to `spark_jar_task`, as it will be removed in version 0.3",
-			Description:   "Deprecated. Please use `spark_jar_task`.",
-			Type:          schema.TypeString,
-			Optional:      true,
-			ConflictsWith: []string{"python_file", "notebook_path", "spark_submit_parameters"},
-		}
-		s["jar_parameters"] = &schema.Schema{
-			Deprecated:  "Please migrate `jar_parameters` to `spark_jar_task`, as it will be removed in version 0.3",
-			Description: "Deprecated. Please use `spark_jar_task`.",
-			Type:        schema.TypeList,
-			Optional:    true,
-			Elem:        &schema.Schema{Type: schema.TypeString},
-		}
-		s["python_file"] = &schema.Schema{
-			Deprecated:    "Please migrate `python_file` to `spark_python_task`, as it will be removed in version 0.3",
-			Description:   "Deprecated. Please use `spark_python_task`.",
-			Type:          schema.TypeString,
-			Optional:      true,
-			ConflictsWith: []string{"jar_main_class_name", "notebook_path", "spark_submit_parameters"},
-		}
-		s["python_parameters"] = &schema.Schema{
-			Deprecated:  "Please migrate `python_parameters` to `spark_python_task`, as it will be removed in version 0.3",
-			Description: "Deprecated. Please use `spark_python_task`.",
-			Type:        schema.TypeList,
-			Optional:    true,
-			Elem:        &schema.Schema{Type: schema.TypeString},
-		}
-		s["spark_submit_parameters"] = &schema.Schema{
-			Deprecated:    "Please migrate `spark_submit_parameters` to `spark_submit_task`, as it will be removed in version 0.3",
-			Type:          schema.TypeList,
-			Optional:      true,
-			Elem:          &schema.Schema{Type: schema.TypeString},
-			ConflictsWith: []string{"jar_main_class_name", "notebook_path", "python_file"},
-		}
-		// legacy library configuration blocks
-		s["library_jar"] = librarySchema("path")
-		s["library_egg"] = librarySchema("path")
-		s["library_whl"] = librarySchema("path")
-		s["library_pypi"] = librarySchema("package", "repo")
-		s["library_cran"] = librarySchema("package", "repo")
-		s["library_maven"] = librarySchema("coordinates", "repo")
-		addMavenExclusions(s["library_maven"])
 		return s
 	})
 
+// ResourceJob ...
 func ResourceJob() *schema.Resource {
-	return &schema.Resource{
+	return util.CommonResource{
+		Schema: jobSchema,
 		SchemaVersion: 2,
-		Create:        resourceJobCreate,
-		Read:          resourceJobRead,
-		Update:        resourceJobUpdate,
-		Delete:        resourceJobDelete,
-		Schema:        jobSchema,
-		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			var js JobSettings
+			err := internal.DataToStructPointer(d, jobSchema, &js)
+			if err != nil {
+				return err
+			}
+			job, err := NewJobsAPI(ctx, c).Create(js)
+			if err != nil {
+				return err
+			}
+			d.SetId(job.ID())
+			return nil
 		},
-	}
-}
-
-func resourceJobCreate(d *schema.ResourceData, m interface{}) error {
-	client := m.(*common.DatabricksClient)
-	jobSettings, err := parseSchemaToJobSettings(d)
-	if err != nil {
-		return err
-	}
-	job, err := NewJobsAPI(client).Create(jobSettings)
-	if err != nil {
-		return err
-	}
-	id := job.ID()
-	d.SetId(id)
-	return resourceJobRead(d, m)
-}
-
-func resourceJobRead(d *schema.ResourceData, m interface{}) error {
-	client := m.(*common.DatabricksClient)
-	job, err := NewJobsAPI(client).Read(d.Id())
-	if ae, ok := err.(common.APIError); ok && ae.IsMissing() {
-		d.SetId("")
-		return nil
-	}
-	if err != nil {
-		return err
-	}
-	return internal.StructToData(*job.Settings, jobSchema, d)
-}
-
-func resourceJobUpdate(d *schema.ResourceData, m interface{}) error {
-	client := m.(*common.DatabricksClient)
-	jobSettings, err := parseSchemaToJobSettings(d)
-	if err != nil {
-		return err
-	}
-	err = NewJobsAPI(client).Update(d.Id(), jobSettings)
-	if err != nil {
-		return err
-	}
-	return resourceJobRead(d, m)
-}
-
-func resourceJobDelete(d *schema.ResourceData, m interface{}) error {
-	client := m.(*common.DatabricksClient)
-	return NewJobsAPI(client).Delete(d.Id())
-}
-
-func parseSchemaToJobSettings(d *schema.ResourceData) (job JobSettings, err error) {
-	err = internal.DataToStructPointer(d, jobSchema, &job)
-	if err != nil {
-		return
-	}
-	if len(job.Libraries) == 0 {
-		cll := legacyReadLibraryListFromData(d)
-		job.Libraries = cll.Libraries
-	}
-	if job.NotebookTask == nil {
-		job.NotebookTask = parseSchemaToNotebookTask(d)
-	}
-	if job.SparkJarTask == nil {
-		job.SparkJarTask = parseSchemaToSparkJarTask(d)
-	}
-	if job.SparkPythonTask == nil {
-		job.SparkPythonTask = parseSchemaToSparkPythonTask(d)
-	}
-	if job.SparkSubmitTask == nil {
-		job.SparkSubmitTask = parseSchemaToSparkSubmitTask(d)
-	}
-	return
-}
-
-// DEPRECATED
-func parseSchemaToNotebookTask(d *schema.ResourceData) *NotebookTask {
-	var got bool
-	var notebookTask NotebookTask
-	if path, ok := d.GetOk("notebook_path"); ok {
-		got = true
-		notebookTask.NotebookPath = path.(string)
-	}
-	if notebookParams, ok := d.GetOk("notebook_base_parameters"); ok {
-		got = true
-		notebookTask.BaseParameters = convertMapStringInterfaceToStringString(notebookParams.(map[string]interface{}))
-	}
-	if !got {
-		return nil
-	}
-	return &notebookTask
-}
-
-// DEPRECATED
-func parseSchemaToSparkJarTask(d *schema.ResourceData) *SparkJarTask {
-	var got bool
-	var sparkJarTask SparkJarTask
-	if uri, ok := d.GetOk("jar_uri"); ok {
-		got = true
-		sparkJarTask.JarURI = uri.(string)
-	}
-	if cName, ok := d.GetOk("jar_main_class_name"); ok {
-		got = true
-		sparkJarTask.MainClassName = cName.(string)
-	}
-	if jarParams, ok := d.GetOk("jar_parameters"); ok {
-		got = true
-		sparkJarTask.Parameters = internal.ConvertListInterfaceToString(jarParams.([]interface{}))
-	}
-	if !got {
-		return nil
-	}
-	return &sparkJarTask
-}
-
-// DEPRECATED
-func parseSchemaToSparkPythonTask(d *schema.ResourceData) *SparkPythonTask {
-	var got bool
-	var sparkPythonTask SparkPythonTask
-	if file, ok := d.GetOk("python_file"); ok {
-		got = true
-		sparkPythonTask.PythonFile = file.(string)
-	}
-	if pythonParams, ok := d.GetOk("python_parameters"); ok {
-		got = true
-		sparkPythonTask.Parameters = internal.ConvertListInterfaceToString(pythonParams.([]interface{}))
-	}
-	if !got {
-		return nil
-	}
-	return &sparkPythonTask
-}
-
-// DEPRECATED
-func parseSchemaToSparkSubmitTask(d *schema.ResourceData) *SparkSubmitTask {
-	var got bool
-	var sparkSubmitTask SparkSubmitTask
-	if sparkSubmitParams, ok := d.GetOk("spark_submit_parameters"); ok {
-		got = true
-		sparkSubmitTask.Parameters = internal.ConvertListInterfaceToString(sparkSubmitParams.([]interface{}))
-	}
-	if !got {
-		return nil
-	}
-	return &sparkSubmitTask
+		Read: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			job, err := NewJobsAPI(ctx, c).Read(d.Id())
+			if err != nil {
+				return err
+			}
+			return internal.StructToData(*job.Settings, jobSchema, d)
+		},
+		Update: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			var js JobSettings
+			err := internal.DataToStructPointer(d, jobSchema, &js)
+			if err != nil {
+				return err
+			}
+			return NewJobsAPI(ctx, c).Update(d.Id(), js)
+		},
+		Delete: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			return NewJobsAPI(ctx, c).Delete(d.Id())
+		},
+	}.ToResource()
 }
